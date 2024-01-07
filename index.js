9dc0b4250b4f7e53a0114c7e8eedd5745c45d1e6
Hamisa Rizgallah <hamisa.rizgallah@student.moringaschool.com>
2024-01-07 20:27:49 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(){
+    const broom = [...cats, "Broom"];
+    return broom;
+}
+
+function prependCat(){
+    const arnold = ["Arnold", ...cats];
+    return arnold;
+}
+
+function removeLastCat(){
+    const last = [...cats.slice(0,2)];
+    return last;
+}
+
+function removeFirstCat(){
+    const first = [...cats.slice(1)];
+    return first;
+}
